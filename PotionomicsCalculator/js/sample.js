101c2868c3adfd3f3511f60c1db5599c582aeca5
Natasha <natasha.pearl08@gmail.com>
2022-11-28 10:17:47 +0000

    convert csv to json and read ingredients data

---
+++ PotionomicsCalculator/js/sample.js
@@ -0,0 +1,38 @@
+$.ajax({
+  url: 'data/ingredients.csv',
+  dataType: 'text',
+}).done(successFunction);
+
+function successFunction(data) {
+  var allRows = data.split(/\r?\n|\r/);
+  let table = "";
+  for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+    if (singleRow === 0) {
+      table += '<thead>';
+      table += '<tr>';
+    } else {
+      table += '<tr>';
+    }
+    var rowCells = allRows[singleRow].split(',');
+    for (var rowCell = 0; rowCell < rowCells.length-1; rowCell++) {
+      if (singleRow === 0) {
+        table += '<th>';
+        table += rowCells[rowCell];
+        table += '</th>';
+      } else {
+        table += '<td>';
+        table += rowCells[rowCell];
+        table += '</td>';
+      }
+    }
+    if (singleRow === 0) {
+      table += '</tr>';
+      table += '</thead>';
+      table += '<tbody>';
+    } else {
+      table += '</tr>';
+    }
+  }
+  table += '</tbody>';
+  $('#sampleTable').append(table);
+}
